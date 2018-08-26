6435579a904e3c92b292ad5e97635591f466fa80
rcardin <riccardo.cardin@gmail.com>
2018-08-26 14:21:08 +0200

    Added the heartbeat timeout to follower actors. Upgrade Akka Typed to 2.5.15

--- build.sbt
+++ build.sbt
@@ -4,4 +4,4 @@ version := "0.1"
 
 scalaVersion := "2.12.6"
 
-libraryDependencies += "com.typesafe.akka" %% "akka-actor-typed" % "2.5.14"
+libraryDependencies += "com.typesafe.akka" %% "akka-actor-typed" % "2.5.15"
--- src/main/scala/io/actorbase/gondola/Raft.scala
+++ src/main/scala/io/actorbase/gondola/Raft.scala
@@ -1,15 +1,23 @@
 package io.actorbase.gondola
 
-import akka.NotUsed
-import akka.actor.typed.{ActorSystem, Behavior, SpawnProtocol, Terminated}
+import java.util.concurrent.TimeUnit
+
 import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorSystem, Behavior, SpawnProtocol}
+
+import scala.concurrent.duration.FiniteDuration
+import scala.util.Random
 
 object Raft {
+
+  private case object TimerKey
+
   sealed trait RaftProtocol
   final case class RequestVote(term: Int) extends RaftProtocol
   final case class Voted(term: Int) extends RaftProtocol
   final case class AppendEntries(term: Int) extends RaftProtocol
   final case class EntriesAppended(term: Int) extends RaftProtocol
+  private final case object CheckHeartbeat extends RaftProtocol
 
   private def candidate: Behavior[RaftProtocol] =
     Behaviors.receivePartial {
@@ -21,17 +29,33 @@ object Raft {
       case (ctx, EntriesAppended(term)) => Behaviors.same
     }
 
-  private def follower: Behavior[RaftProtocol] =
-    Behaviors.receivePartial {
-      case (ctx, RequestVote(term)) => Behaviors.same
-      case (ctx, AppendEntries(term)) => Behaviors.same
+  private def follower(heartbeat: Long, lastHeartbeat: Long): Behavior[RaftProtocol] =
+    Behaviors.withTimers { timers =>
+      timers.startPeriodicTimer(TimerKey, CheckHeartbeat, FiniteDuration.apply(heartbeat, TimeUnit.MILLISECONDS))
+      Behaviors.receivePartial {
+        case (ctx, RequestVote(term)) => Behaviors.same
+        case (ctx, AppendEntries(term)) => Behaviors.same
+        case (ctx, CheckHeartbeat) =>
+          if (now() - lastHeartbeat > heartbeat) {
+            candidate
+          }
+          Behaviors.same
+      }
     }
 
   val main: Behavior[SpawnProtocol] =
     Behaviors.setup { ctx =>
-      val followerActor = ctx.spawn(follower, "follower-1")
+      val followerActor = ctx.spawn(follower(randomHeartbeat(), now()), "follower-1")
       SpawnProtocol.behavior
     }
 
   val system = ActorSystem(main, "Raft")
+
+  def now(): Long = {
+    System.currentTimeMillis()
+  }
+
+  def randomHeartbeat(): Long = {
+    150 + Random.nextInt(150)
+  }
 }
