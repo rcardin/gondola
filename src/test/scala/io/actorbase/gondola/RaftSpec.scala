e77c951cf3ddc1849135ebc7e3846376994c699a
rcardin <riccardo.cardin@gmail.com>
2018-09-02 14:18:28 +0200

    Added check heartbeat test for follower actors

--- src/test/scala/io/actorbase/gondola/RaftSpec.scala
+++ src/test/scala/io/actorbase/gondola/RaftSpec.scala
@@ -1,13 +1,15 @@
 package io.actorbase.gondola
 
 import akka.actor.testkit.typed.scaladsl.{ActorTestKit, ManualTime, TestProbe}
+import com.typesafe.config.Config
 import io.actorbase.gondola.Raft._
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
+
 import scala.concurrent.duration._
 
 class RaftSpec extends WordSpec with ActorTestKit with Matchers with BeforeAndAfterAll {
 
-  override def config = ManualTime.config
+  override def config: Config = ManualTime.config
   val manualTime: ManualTime = ManualTime()
 
   override protected def afterAll(): Unit = {
