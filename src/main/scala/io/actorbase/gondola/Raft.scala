4d998d2fd7e4fe75c395eae8813bf4c54d3d1f95
rcardin <riccardo.cardin@gmail.com>
2018-09-10 21:37:13 +0200

    Continuing to implement candidate actor

--- src/main/scala/io/actorbase/gondola/Raft.scala
+++ src/main/scala/io/actorbase/gondola/Raft.scala
@@ -2,6 +2,7 @@ package io.actorbase.gondola
 
 import java.util.concurrent.TimeUnit
 
+import akka.NotUsed
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior, SpawnProtocol}
 
@@ -13,22 +14,39 @@ object Raft {
   private case object TimerKey
 
   sealed trait RaftProtocol
-  final case class RequestVote(term: Int) extends RaftProtocol
-  final case class Voted(term: Int) extends RaftProtocol
+  final case class RequestVote(term: Int, replyTo: ActorRef[RaftProtocol]) extends RaftProtocol
+  final case class Voted(voted: Boolean, term: Int) extends RaftProtocol
   final case class AppendEntries(term: Int, replyTo: ActorRef[RaftProtocol]) extends RaftProtocol
   final case class HeartbeatResponse(term: Int) extends RaftProtocol
   final case class EntriesAppended(term: Int) extends RaftProtocol
   private[gondola] final case object CheckHeartbeat extends RaftProtocol
 
-  def candidate(nodes: List[ActorRef[RaftProtocol]]): Behavior[RaftProtocol] =
-    // TODO Here we have to use the cameo pattern (per session child actor in Akka typed)
-    Behaviors.receivePartial {
-      case (ctx, Voted(term)) => Behaviors.same
+  // TODO INSERT Election timeout
+  def candidate(nodes: List[ActorRef[RaftProtocol]], currentTerm: Int): Behavior[RaftProtocol] = {
+    val quorum: Int = nodes.size / 2 + 1
+    Behaviors.setup[RaftProtocol] { ctx =>
+      var requestVoteResponses: List[Voted] = Nil
+      nodes.foreach(node => node ! RequestVote(currentTerm + 1, ctx.self))
+
+      def nextBehavior: Behavior[RaftProtocol] = {
+        if (requestVoteResponses.count(_.voted) >= quorum) {
+          leader(nodes)
+        } else {
+          Behaviors.same
+        }
+      }
+
+      Behaviors.receivePartial {
+        case (_, Voted(voted, term)) =>
+          requestVoteResponses = Voted(voted, term) :: requestVoteResponses
+          nextBehavior
+      }
     }
+  }
 
   def leader(nodes: List[ActorRef[RaftProtocol]]): Behavior[RaftProtocol] =
     Behaviors.receivePartial {
-      case (ctx, EntriesAppended(term)) => Behaviors.same
+      case (_, EntriesAppended(term)) => Behaviors.same
     }
 
   def follower(nodes: List[ActorRef[RaftProtocol]],
@@ -38,7 +56,7 @@ object Raft {
     Behaviors.withTimers { timers =>
       timers.startPeriodicTimer(TimerKey, CheckHeartbeat, FiniteDuration.apply(heartbeat, TimeUnit.MILLISECONDS))
       Behaviors.receivePartial {
-        case (ctx, RequestVote(term)) => Behaviors.same
+        case (ctx, RequestVote(term, replyTo)) => Behaviors.same
         case (ctx, AppendEntries(term, leader)) =>
           if (term == currentTerm) {
             leader ! HeartbeatResponse(currentTerm)
@@ -49,14 +67,14 @@ object Raft {
           }
         case (ctx, CheckHeartbeat) =>
           if (now() - lastHeartbeat > heartbeat) {
-            candidate(nodes)
+            candidate(nodes, currentTerm)
           }
           Behaviors.same
       }
     }
 
   val main: Behavior[SpawnProtocol] =
-    // FIXME Nil is not a correct initial value
+  // FIXME Nil is not a correct initial value
     Behaviors.setup { ctx =>
       val followerActor = ctx.spawn(follower(Nil, randomHeartbeat(), now(), 0), "follower-1")
       SpawnProtocol.behavior
